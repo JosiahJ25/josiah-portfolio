f09112686b2d5e75e34ae1bddb07f90a8fbc70c7
JosiahJ25 <jjohnson124@rams.wssu.edu>
2026-06-28 22:44:28 -0400

    Fix Vite base path for local + GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/josiah-portfolio/",
+  base: process.env.NODE_ENV === "production" ? "/josiah-portfolio/" : "/",
 });
