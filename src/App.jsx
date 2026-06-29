cb693a8210a918e3a377bccdca22a39e60622273
JosiahJ25 <jjohnson124@rams.wssu.edu>
2026-06-29 10:35:56 -0400

    Remove duplicate router and fix routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
+import { Routes, Route } from "react-router-dom";
 
 import Home from "./pages/Home";
 import Resume from "./pages/Resume";
@@ -8,16 +8,15 @@ import Contact from "./pages/Contact";
 
 function App() {
   return (
-    <BrowserRouter>
+    <>
       <Navbar />
-
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/resume" element={<Resume />} />
         <Route path="/projects" element={<Projects />} />
         <Route path="/contact" element={<Contact />} />
       </Routes>
-    </BrowserRouter>
+    </>
   );
 }
 
