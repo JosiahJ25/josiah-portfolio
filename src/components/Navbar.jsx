bef263a9660a2db095d21e2485854d9636c3c076
JosiahJ25 <jjohnson124@rams.wssu.edu>
2026-06-29 10:25:15 -0400

    Fix router basename + navbar links

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,12 @@
+import { Link } from "react-router-dom";
+
 export default function Navbar() {
   return (
     <nav style={{ display: "flex", gap: "20px", padding: "20px" }}>
-      <a href="/">Home</a>
-      <a href="/resume">Resume</a>
-      <a href="/projects">Projects</a>
-      <a href="/contact">Contact</a>
+      <Link to="/">Home</Link>
+      <Link to="/resume">Resume</Link>
+      <Link to="/projects">Projects</Link>
+      <Link to="/contact">Contact</Link>
     </nav>
   );
 }
